c85f5144a351ac68f79c8429274a4ab88e51b411
Victoria <Victoria23miha@gmail.com>
2025-02-06 15:19:35 +0300

    Изменение шаблонов

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,22 +1,34 @@
 from django import forms
-
+from django.forms import ModelChoiceField
 
 from .models import Reservation, Table, Message
 
-
-class ReservationForm(forms.ModelForm):
+class ReservationForm (forms.ModelForm):
+    #tables = Table.objects.all()
+    #print(* tables)
+    #table = ModelChoiceField(queryset=tables, widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Reservation
-        fields = ["date", "time", "guests", "table"]
-        exclude = ("owner",)
+        fields = ['date','time','guests','table']
+        exclude = ('owner',)
+
+
+    # def __init__(self, *args, **kwargs):
+    #     super(ReservationForm, self).__init__(*args, **kwargs)
+    #     self.fields['table'].queryset = Table.objects.all()
 
     def __str__(self):
         return self.table
 
+    # def __init__(self, *args, **kwargs):
+    #     user = kwargs.pop('user', '')
+    #     super(Table, self).__init__(*args, **kwargs)
+    #     self.fields['description'] = forms.ModelChoiceField(queryset=Table.objects.filter(owner=user))
 
-class MessageForm(forms.ModelForm):
+class MessageForm (forms.ModelForm):
 
     class Meta:
         model = Message
-        fields = ["name", "email", "text"]
+        fields = ['name','email','text']
+        #exclude = ('',)
--- reservations/models.py
+++ reservations/models.py
@@ -1,8 +1,8 @@
+#from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
-
 
 class Table(models.Model):
     number = models.PositiveIntegerField(unique=True)
@@ -10,8 +10,7 @@ class Table(models.Model):
     description = models.TextField(blank=True)
 
     def __str__(self):
-        return f"Table {self.number} - {self.seats} seats"
-
+        return f"Стол {self.number} - {self.seats} место"
 
 class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,12 +23,12 @@ class Reservation(models.Model):
         max_length=100,
         blank=True,
         null=True,
+        #verbose_name=_("Token")
     )
     is_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Reservation for {self.user} on {self.date} at {self.time} ({self.guests} person(s)"
-
 
 class Message(models.Model):
     name = models.TextField(blank=True)
